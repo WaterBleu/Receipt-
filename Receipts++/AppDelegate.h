ce31f87c2f343f5c29bcdb7b6e3785aeb14ce221
WaterBleu <jeff7746@hotmail.com>
2015-07-21 10:51:40 -0700

    Initial Commit

---
+++ Receipts++/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  Receipts++
+//
+//  Created by Jeff Huang on 2015-07-21.
+//  Copyright (c) 2015 Jeff Huang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
